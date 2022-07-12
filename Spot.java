ef76e012118413d3658e4d82d2100164d42e4d76
Umay ŞAMLI <76824752+samliumay@users.noreply.github.com>
2022-07-12 12:23:10 +0300

    Add files via upload

---
+++ Spot.java
@@ -0,0 +1,35 @@
+import Pieces.*;
+
+public class Spot {
+    private Piece piece;
+    private int x;
+    private int y;
+
+    public Spot (int x, int y, Piece piece)
+    {
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
